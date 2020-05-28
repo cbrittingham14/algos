a4389551b3c0690c5f248fb8682361ca5d29eaad
chizz <cbrittingham@gmail.com>
2020-05-28 14:12:04 -0500

    copied in new folder to work on

---
+++ 04-caesar-cipher/Unsolved/caesar-cipher.js
@@ -0,0 +1,10 @@
+// Write a function that takes in a string and and an offset and returns a new string with each character moved by the offset
+
+const caesarCipher = function(str, offset) {
+  offset = offset % 26;
+
+  console.log(offset);
+};
+
+
+caesarCipher('av', 5);
