6af760cd0ab5624e61d26ec59e58a8578819027f
chizz <cbrittingham@gmail.com>
2020-05-28 15:44:25 -0500

    cleaned up code slightly

--- 04-caesar-cipher/Unsolved/caesar-cipher.js
+++ 04-caesar-cipher/Unsolved/caesar-cipher.js
@@ -6,10 +6,8 @@ const caesarCipher = function(str, offset) {
   // the closed loop of the alphabet
   offset = offset % 26; 
 
-  // convert the alphabet and given string to arrays
-  // for easier manipulation
+  //assign alphabet and convert string to array to use map method
   const alpha = 'abcdefghijklmnopqrstuvwxyz';
-  const alphArray = alpha.split('');
   const strArray = str.split('');
 
   const caesar = strArray.map( l => {
@@ -20,9 +18,9 @@ const caesarCipher = function(str, offset) {
     }
 
     let lc = l.toLowerCase(); // make letter lowercase for comparison
-    let shiftedIndex = alphArray.indexOf(lc) + offset; // get index of shifted letter
+    let shiftedIndex = alpha.indexOf(lc) + offset; // get index of shifted letter
+    console.log('alpha' ,alpha.indexOf(lc));
 
-    
     // set bool in order to return a shifted letter
     // in the same case as it was received
     let isUpperCase = false;
@@ -32,14 +30,13 @@ const caesarCipher = function(str, offset) {
     // check if shift is outside alphabet index and adjust accordingly
     if (shiftedIndex > 25) {
       let index = shiftedIndex - 26;
-      return (isUpperCase ? alphArray[index].toUpperCase() : alphArray[index]);
+      return (isUpperCase ? alpha[index].toUpperCase() : alpha[index]);
     } else if (shiftedIndex < 0){
       let index = shiftedIndex + 26;
-      return (isUpperCase ? alphArray[index].toUpperCase() : alphArray[index]);
+      return (isUpperCase ? alpha[index].toUpperCase() : alpha[index]);
     } else {
-      return (isUpperCase ? alphArray[shiftedIndex].toUpperCase() : alphArray[shiftedIndex]);
+      return (isUpperCase ? alpha[shiftedIndex].toUpperCase() : alpha[shiftedIndex]);
     }
   });
-  
   return caesar.join('');
 };
